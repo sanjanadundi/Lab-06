e6ddc7c125992f929b1452cbba6e5d9cda32d17e
chloe-miller-13 <chloelauryn13@gmail.com>
2023-03-22 15:57:04 -0400

    Update 3: Chloe added decode_pass

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 def encode_pass(password):
-    new_pass = ''
+    new_pass = ""
     for num in password:
          new_pass += str((int(num) + 3) % 10)
     return new_pass
 
 
 def decode_pass(password):
-    pass_nums = password.split()
-    for num in pass_nums:
-        num = int(num) - 3
-    for num in pass_nums:
-        pass_nums += str(num)
-    return pass_nums
+    decoded_pass = ''
+    for num in password:
+        if num == "2":
+            decoded_pass += "9"
+        elif num == "1":
+            decoded_pass += "8"
+        elif num == "0":
+            decoded_pass += "7"
+        else:
+            decoded_pass += str((int(num) - 3) % 10)
+    return decoded_pass
 
 
 def main():
@@ -22,18 +27,18 @@ def main():
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
+        print()
         option = int(input("Please enter an option: "))
         if option == 1:
             user_password = input("Please enter a password to encode: ")
             encoded_pass = encode_pass(user_password)
-            print(encoded_pass)
             print("Your password has been encoded and stored!")
+            print()
         if option == 2:
-            decoded_pass = decode_pass(user_password)
-            print(f'The encoded password is {encoded_pass}, and the original password is {user_password}')
-    else:
-        exit
-
+            decoded_pass = decode_pass(encoded_pass)
+            print(f'The encoded password is {encoded_pass}, and the original password is {decoded_pass}.')
+        if option == 3:
+            break
 
 
 if __name__ == "__main__":
